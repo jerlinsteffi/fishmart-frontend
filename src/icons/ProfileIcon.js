7907bf94878ef046f1e39615149fb02f86d93ddc
jerlinsteffi <steffiarulsamy@gmail.com>
2025-08-12 12:03:14 +0530

    header

---
+++ src/icons/ProfileIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ProfileIcon = ({ width = 25, height = 25 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#4ca626"
+    viewBox="0 0 24 24"
+    width={width}
+    height={height}
+  >
+    <path d="M12 12c2.7 0 4.9-2.2 4.9-4.9S14.7 2.2 12 2.2 7.1 4.4 7.1 7.1 9.3 12 12 12zm0 2.2c-3.1 0-9.3 1.6-9.3 4.7v1.6h18.6v-1.6c0-3.1-6.2-4.7-9.3-4.7z" />
+  </svg>
+);
--- src/libs/dashboard-layout/layout.tsx
+++ src/libs/dashboard-layout/layout.tsx
@@ -1,9 +1,10 @@
 import { LayoutProps } from "@/types/page";
+import Header from "../ui/Header";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="d-flex flex-column min-vh-100">
-      <h1>Header</h1>
+      <Header />
       <main className="flex-grow-1">{children}</main>
       <h1>Footer</h1>
     </div>
